f214b33211aaa5216c2a252ed2bcaac2bdabdadf
chapapau8u492u <chapapau8u492u@outlook.com>
2025-07-09 18:39:30 +0530

    allow cors

--- server.js
+++ server.js
@@ -19,11 +19,7 @@ MongoClient.connect(MONGODB_URI)
   .catch(error => console.error('MongoDB connection error:', error));
 
 // Middleware
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Accept', 'Origin']
-}));
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 
 // Routes
