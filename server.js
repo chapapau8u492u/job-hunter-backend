e4f54760f8e188da3675b9d355c9d459485bf854
chapapau8u492u <chapapau8u492u@outlook.com>
2025-07-10 21:02:10 +0530

    link in cors

--- server.js
+++ server.js
@@ -53,7 +53,8 @@ app.use(cors({
     'https://preview--application-ace-platform.lovable.app',
     /\.lovableproject\.com$/,
     /\.lovable\.app$/,
-    `https://preview--career-growth-orchestrator-85.lovable.app/`
+    `https://preview--career-growth-orchestrator-85.lovable.app/,
+    https://student-os-zeta.vercel.app`
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Accept', 'Origin', 'Authorization'],
